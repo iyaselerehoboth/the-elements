7d20a6b7c76adaf9c119af987dd5dc272d00aea8
Rehoboth <rehobothi@yahoo.com>
2020-10-12 23:47:15 +0100

    Began app development

---
+++ app/src/main/java/com/iyaselerehoboth/theelements/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.iyaselerehoboth.theelements;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.iyaselerehoboth.theelements.databinding.ActivitySplashscreenBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    ActivitySplashscreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splashscreen);
+
+        binding.btnBegin.setOnClickListener(view -> {
+            //Do Something
+            startActivity(new Intent(this, HomePageActivity.class));
+        });
+    }
+}

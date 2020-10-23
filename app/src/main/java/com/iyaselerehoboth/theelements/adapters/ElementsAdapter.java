89f61b20ee8010c6bbc3687d5ab50a96918090f3
Rehoboth <rehobothi@yahoo.com>
2020-10-23 01:03:21 +0100

    Implemented search for SearchFragment

--- app/src/main/java/com/iyaselerehoboth/theelements/adapters/ElementsAdapter.java
+++ app/src/main/java/com/iyaselerehoboth/theelements/adapters/ElementsAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,16 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.iyaselerehoboth.theelements.data.room.tables.Elements;
 import com.iyaselerehoboth.theelements.databinding.ItemElementsLayoutBinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ElementsAdapter extends RecyclerView.Adapter<ElementsAdapter.ViewHolder> {
+public class ElementsAdapter extends RecyclerView.Adapter<ElementsAdapter.ViewHolder> implements Filterable {
     private final OnItemClickListener listener;
     private Context mCtx;
     private List<Elements> elementsList;
+    private List<Elements> mFilteredList;
 
     public ElementsAdapter(Context context, List<Elements> elementsList, OnItemClickListener listener) {
         this.mCtx = context;
         this.elementsList = elementsList;
+        this.mFilteredList = elementsList;
         this.listener = listener;
     }
 
@@ -33,12 +38,47 @@ public class ElementsAdapter extends RecyclerView.Adapter<ElementsAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(elementsList.get(position));
+        holder.bind(mFilteredList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return elementsList.size();
+        if (mFilteredList != null) {
+            return mFilteredList.size();
+        } else {
+            return elementsList.size();
+        }
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    mFilteredList = elementsList;
+                } else {
+                    List<Elements> filteredList = new ArrayList<>();
+                    for (Elements element : elementsList) {
+                        if (element.getName().toLowerCase().contains(charString)
+                                || element.getSymbol().toLowerCase().contains(charString) || element.getAtomic_number().toString().contains(charString)) {
+                            filteredList.add(element);
+                        }
+                    }
+                    mFilteredList = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mFilteredList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mFilteredList = (List<Elements>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     public interface OnItemClickListener {
--- app/src/main/java/com/iyaselerehoboth/theelements/views/fragments/SearchFragment.java
+++ app/src/main/java/com/iyaselerehoboth/theelements/views/fragments/SearchFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,7 +18,7 @@ import com.iyaselerehoboth.theelements.data.SessionManager;
 import com.iyaselerehoboth.theelements.data.room.TheElementsDatabase;
 import com.iyaselerehoboth.theelements.databinding.FragmentSearchBinding;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements SearchView.OnQueryTextListener {
     FragmentSearchBinding binding;
     SessionManager session;
     TheElementsDatabase db;
@@ -41,6 +42,8 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.svElement.setOnQueryTextListener(this);
+
         db.getElementsDao().getAllElements().observe(getViewLifecycleOwner(), elementsList -> {
 
             adapter = new ElementsAdapter(getActivity(), elementsList, element -> {
@@ -50,5 +53,20 @@ public class SearchFragment extends Fragment {
             binding.rvElements.setLayoutManager(new LinearLayoutManager(getActivity()));
             binding.rvElements.setAdapter(adapter);
         });
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        try {
+            adapter.getFilter().filter(s.toLowerCase());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }

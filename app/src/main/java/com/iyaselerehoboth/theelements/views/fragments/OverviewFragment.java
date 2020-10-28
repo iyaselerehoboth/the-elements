5c08434bfb2ed7c8c125e712a6eeab065c6807d2
Rehoboth <rehobothi@yahoo.com>
2020-10-29 00:32:04 +0100

    Replaced image-view in overview with pdf-viewer

--- app/src/main/java/com/iyaselerehoboth/theelements/views/fragments/OverviewFragment.java
+++ app/src/main/java/com/iyaselerehoboth/theelements/views/fragments/OverviewFragment.java
@@ -10,10 +10,9 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.github.barteksc.pdfviewer.util.FitPolicy;
 import com.iyaselerehoboth.theelements.R;
 import com.iyaselerehoboth.theelements.databinding.FragmentOverviewBinding;
-
-import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
 
 public class OverviewFragment extends Fragment {
     FragmentOverviewBinding binding;
@@ -35,8 +34,26 @@ public class OverviewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.imgZoom.setDisplayType(ImageViewTouchBase.DisplayType.FIT_TO_SCREEN);
-        binding.imgZoom.setImageResource(R.drawable.periodic_elements_black);
+        /*binding.imgZoom.setDisplayType(ImageViewTouchBase.DisplayType.FIT_TO_SCREEN);
+        binding.imgZoom.setImageResource(R.drawable.periodic_elements_black);*/
+
+        binding.pdfViewer.fromAsset("NaturalStatePeriodicTable.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .spacing(0)
+                .autoSpacing(false)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
+                .pageSnap(false) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
 
     }
 }
